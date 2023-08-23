32e2c13babf44b7963efaec0548d5b1143bd0e68
swekenyi <41902897+swekenyi@users.noreply.github.com>
2023-08-23 20:57:58 +0300

    0x06-pointers_arrays_strings

---
+++ 0x06-pointers_arrays_strings/README.md
@@ -0,0 +1,1 @@
+My readme or pointers arrays
---
+++ 0x06-pointers_arrays_strings/main.h
@@ -0,0 +1,18 @@
+#ifndef _MAIN_H_
+#define _MAIN_H__
+
+char *_strncpy(char *dest, char *src, int n);
+char *_strcat(char *dest, char *src);
+char *_strncat(char *dest, char *src, int n);
+char *cap_string(char *);
+char *leet(char *);
+int _strcmp(char *s1, char *s2);
+char *infinite_add(char *n1, char *n2, char *r, int size_r);
+char *string_toupper(char *);
+int _putchar(char);
+char *rot13(char *);
+void print_number(int n);
+void print_buffer(char *b, int size);
+void reverse_array(int *a, int n);
+
+#endif
